18179ce3df466e482928daf03fc1dcaf9bcc9db9
Satwik-vinay-teja <86521807+Satwik-vinay-teja@users.noreply.github.com>
2021-06-28 13:17:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This waas an amazing bootcamp conducted by Shaurya , We have covered
+        basic knowledge about webdevelopment
+      </p>
+    </div>
+  );
+}
+
+export default Info;
